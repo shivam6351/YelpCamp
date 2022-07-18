c0fa5f75e7d767828e8d27e1511c03377a3558f5
Shivam <2020mcb1250@iitrpr.ac.in>
2022-07-18 11:02:56 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.flash('error','you must be signed in ')
         req.session.returnTo = req.originalUrl;
-        console.log(req.session.returnTo);
         return res.redirect('/login');
     }
     next();
