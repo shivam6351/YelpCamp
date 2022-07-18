3ef7937b4affdb5cfc3c0dd4c4a888c5c90c4ef2
Shivam <2020mcb1250@iitrpr.ac.in>
2022-07-18 12:11:13 +0530

    authentication

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
 const session  = require('express-session')
+const {campgroundSchema,reviewSchema}=require('./schemas.js');
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground')
+const Review = require('./models/review')
 
 
 module.exports.isLoggedIn = (req,res,next)=>{
@@ -6,6 +10,49 @@ module.exports.isLoggedIn = (req,res,next)=>{
         req.flash('error','you must be signed in ')
         req.session.returnTo = req.originalUrl;
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next();
+    }
+}
+
+
+module.exports.isAuthor = async (req,res,next)=>{
+    const {id} = req.params
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error','you do not have permission to do that');
+        return  res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+
+module.exports.validateReview = (req,res,next)=>{
+    const {error}=reviewSchema.validate(req.body);
+    if(error){
+        const msg=error.details.map(el=> el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next();
+    }
+}
+module.exports.isReviewAuthor = async (req,res,next)=>{
+    const {id,reviewId} = req.params
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error','you do not have permission to do that');
+        return  res.redirect(`/campgrounds/${id}`)
     }
     next();
 }
