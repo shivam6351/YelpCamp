6444d34aab4707bce1acb670a21f593bca225c81
Shivam <2020mcb1250@iitrpr.ac.in>
2022-07-19 15:07:52 +0530

    image upload

--- models/campground.js
+++ models/campground.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose');
 const Review = require('./review');
 
+
+//https://res.cloudinary.com/dcccwdpas/image/upload/v1658213378/YelpCamp/ct0xvtv3625ucabrebsp.jpg
+
+
 const Schema=mongoose.Schema;
+
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_200')
+})
 
 const CampgroundSchema= new Schema({
     title: String,
-    images: [
-        {
-        url: String,
-        filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
