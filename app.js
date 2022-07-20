a02687057da9174b900403b8b864f2601cf7e3cc
Shivam <2020mcb1250@iitrpr.ac.in>
2022-07-20 15:20:12 +0530

    deploying

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
     res.send("OH boy something went wrong");
 })
-
-app.listen(3000, () => {
-    console.log("Serving on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
